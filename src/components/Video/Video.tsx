745947ab82a22ba9c87c9c31720d564b95c07297
João Pedro de Menezes Pontes Fernandes <joao.pedro.fernandes.122@gmail.com>
2022-06-24 17:25:11 -0300

    Criando a descrição e os botões

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,7 +1,34 @@
+import { DiscordLogo, Lightning } from "phosphor-react";
+
 export function Video(){
   return(
     <div className="flex-1">
-      <span>video</span>
+      <div className="bg-black flex justify-center">
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+      </div>
+      <div className="p-8 max-w-[1100px] mx-auto">
+        <div className="flex items-start gap-16"> 
+          <div className="flex-1">
+            <h1 className="text-2xl font-bold">
+              Aula 01-Abertura do ignite lab
+            </h1>
+            <p className="mt-4 text-gray-200 leading-relaxed">
+              descrição
+            </p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <a href="" className="p-4 text-sm bg-green-500 flex items-start rounded font bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
+              <DiscordLogo size={24} />
+              Comunidade do discord
+            </a>
+
+            <a href="" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-start rounded font bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
+              <Lightning size={24} />
+              Acesse o desfio
+            </a>
+          </div>
+        </div>  
+      </div>
     </div>
   )
 }
