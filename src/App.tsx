287f26848b70fee1635d5263e16e229c9c902ef6
João Pedro de Menezes Pontes Fernandes <joao.pedro.fernandes.122@gmail.com>
2022-06-24 15:51:04 -0300

    Colocando barra lateral na pag principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { gql, useQuery } from "@apollo/client"
+import { Header } from "./components/Header/Header"
+import { SideBar } from "./components/SideBar/SideBar"
+import { Video } from "./components/Video/Video"
 import { Event } from "./pages/Event"
 
 function App() {
 
   return (
-    <>
-      <Event />   
-    </>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex flex-1">
+        <Video />
+        <SideBar />
+      </main>
+    </div>
   )
 }
 
