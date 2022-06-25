84b42c1bf3340715b5178d90b0c2020cc8c881e3
João Pedro de Menezes Pontes Fernandes <joao.pedro.fernandes.122@gmail.com>
2022-06-25 14:47:23 -0300

    Mudando as variaveis de ambiente

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4q6qj3e3e5d01w7068p30tj/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers:{
+    'Authotization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
